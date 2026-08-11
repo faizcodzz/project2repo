d004867a1a4b10c1972fd3544166cfd2f0103c96
unknown871786 <sahilmohammad871@gmail.com>
2026-08-11 13:53:24 +0530

    Initial Django project

--- todo/views.py
+++ todo/views.py
@@ -7,9 +7,26 @@ def task_list(request):
     return render(request,'todo/task_list.html',{'task':task})
 
 
-# def task_create(request):
-#     if request,method =='POST':
-#         title = request.POST.get('title','').strip()
-#         description = request.POST.get('description','').strip()
-#         if title:
-#             Task.objects.create(title=title, description=description)
+def task_create(request):
+    if request,method =='POST':
+        title = request.POST.get('title','').strip()
+        description = request.POST.get('description','').strip()
+        if title:
+            Task.objects.create(title=title, description=description)
+            return redirect(reverse('todo:task_list'))
+        error = 'title cannot be empty'
+        return render (request,'todo/task_form.html',{'error':error})
+    return render(request,'todo/task_form.html')
+
+def task_update(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    if request,method =='POST':
+        title = request.POST.get('title','').strip()
+        description = request.POST.get('description','').strip()
+        completed = request.POST.get('completed')=='on'
+        if title:
+            task.title=title
+            task.description=description
+            task.completed=completed
+            task.save()
+            return redirect(reverse('todo:task_list'))
